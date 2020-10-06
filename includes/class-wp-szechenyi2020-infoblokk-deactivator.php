d58eda6367e283f605659fa6cf6457f0465d20c5
Balint Kovacs <balint@y-collective.hu>
2020-10-07 00:07:46 +0200

    add base boilerplate
    refs #1

---
+++ includes/class-wp-szechenyi2020-infoblokk-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bolint.hu
+ * @since      1.0.0
+ *
+ * @package    Wp_Szechenyi2020_Infoblokk
+ * @subpackage Wp_Szechenyi2020_Infoblokk/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Szechenyi2020_Infoblokk
+ * @subpackage Wp_Szechenyi2020_Infoblokk/includes
+ * @author     Balint Kovacs <balint.kovacs@bolint.hu>
+ */
+class Wp_Szechenyi2020_Infoblokk_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
